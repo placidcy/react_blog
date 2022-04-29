1a075142fa6397e1579c5fa572f723a61808fd63
placidcy <97069998+placidcy@users.noreply.github.com>
2022-04-29 18:01:14 +0900

    11
    11

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Join from "./pages/join/Join";
 import Write from "./pages/write/Write";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as BrowserRouter, Switch, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import View from "./pages/view/View";
 import "./App.css";
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <Header />
             <Switch>
                 <Route path="/">
@@ -35,7 +35,7 @@ function App() {
                 </Route>
             </Switch>
             <Footer />
-        </Router>
+        </BrowserRouter>
     );
 }
 
